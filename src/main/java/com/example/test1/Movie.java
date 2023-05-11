11c1104f93ed59769079bbe38fce381db676be04
awayYourHeart <2728557244@qq.com>
2023-05-11 21:23:13 +0800

    同步

---
+++ src/main/java/com/example/test1/Movie.java
@@ -0,0 +1,39 @@
+package com.example.test1;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private  Long id;
+    @Column
+    private String name;
+    @Column
+    private String author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into movie values(1,'红高粱','张艺谋');
+insert into movie values(2,'让子弹飞一会','姜文');
